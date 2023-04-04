8cb3486a286b359418ae1e4378e9873a3936fe02
Nyasha Marufu <55029891+Unstated@users.noreply.github.com>
2023-04-04 06:37:36 -0400

    Adds second half of the game art.

---
+++ Enemy/Large.gd
@@ -0,0 +1,15 @@
+extends Area2D
+var spin = 100
+export var turn = 1
+export var Min = 1
+export var Max = 2
+onready var rate := $turnrate
+
+func _ready():
+	randomize()
+
+func _process(delta):
+	if rate.is_stopped():
+		turn *= -1
+		rate.start(rand_range(Min,Max))
+	rotation_degrees += spin * turn * delta 
