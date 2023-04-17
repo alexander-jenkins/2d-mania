e2065acbff2afadb5efa88671bc27ab81e9d42ff
Alexander Jenkins <alex@alexjenkins.dev>
2023-04-17 14:03:14 -0400

    Bump to Godot 4 project.
    Godot 4 stable release is out, bump project to be a v4 project. Update Git VCS plugin.

--- scripts/enemies/Large.gd
+++ scripts/enemies/Large.gd
@@ -1,9 +1,9 @@
 extends Area2D
 var spin = 100
-export var turn = 1
-export var Min = 1
-export var Max = 2
-onready var rate := $turnrate
+@export var turn = 1
+@export var Min = 1
+@export var Max = 2
+@onready var rate := $turnrate
 
 func _ready():
 	randomize()
@@ -11,5 +11,5 @@ func _ready():
 func _process(delta):
 	if rate.is_stopped():
 		turn *= -1
-		rate.start(rand_range(Min,Max))
+		rate.start(randf_range(Min,Max))
 	rotation_degrees += spin * turn * delta 
