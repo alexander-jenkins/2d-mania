2199b31fb2ba0757bd56727d82093b1c5cf42b78
Alexander Jenkins <alex@alexjenkins.dev>
2023-05-11 21:05:13 -0400

    Add polygon player and script. Add level scene.

---
+++ scripts/player/Polygon.gd
@@ -0,0 +1,16 @@
+extends Player
+
+@export var speed = 10
+var velocity = Vector2.ZERO
+@onready var view = get_viewport()
+
+
+func _ready():
+	pass
+	
+func _physics_process(_delta):
+	var direction = Input.get_vector("Move_left", "Move_right", "Move_up", "Move_down")
+	velocity = direction * speed
+	position.x = clamp(position.x, 0, view.size.x)
+	position.y = clamp(position.y, 0, view.size.y)
+	translate(velocity)
